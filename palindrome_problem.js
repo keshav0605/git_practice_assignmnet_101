659f3f0103a88172cb02e4e96d96ba2f2ef27c13
keshav0605 <keshav06patel@gmail.com>
2022-09-13 23:51:35 +0530

    updated palindrome code

--- palindrome_problem.js
+++ palindrome_problem.js
@@ -1,1 +1,12 @@
 let str ="abcba";
+
+let rev_str = "";
+
+for(let x = str.length-1 ; x <= 0 ; x--){
+    rev_str = rev_str + str[x];
+}
+if(str == rev_str){
+    console.log("It is a Palindrome");
+}else{
+    console.log("It is not a Palindrome");
+}
